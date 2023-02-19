fcce73b20b322b691734c9e67ff6cea7cc563f5c
ViktorSolomianiy <xmegasystem35@gmail.com>
2023-02-19 18:25:55 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import * as API from '../services/api';
 
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 import { Searchbar } from './Searchbar/Searchbar';
